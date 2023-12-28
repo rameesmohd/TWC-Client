4d12a34ff57d23d61a9c19b26d09196dcb4cb71d
Rameez <rameesmohd789@gmail.com>
2023-12-28 17:04:57 +0530

    TWC-Client

---
+++ Client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-green-700 font-bold '>
+          Hello World
+      </div>
+    </>
+  )
+}
+
+export default App
