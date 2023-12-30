b018429a9104d7d041660bf5f8ec98d26079ca1b
Rameez <rameesmohd789@gmail.com>
2023-12-30 08:52:33 +0530

    added axios

--- Client/src/Axios/Useraxios.js
+++ Client/src/Axios/Useraxios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const Useraxios = axios.create({
-    baseURL: 'https://some-domain.com/api/',
+    baseURL: 'https://tradewalker/api/',
     timeout: 1000,
     headers: {'X-Custom-Header': 'foobar'}
   });
